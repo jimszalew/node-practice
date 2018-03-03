622ef07de8945804cef524e330e43f3cb3e89f1f
Jim Szalewski <jim.szalewski@gmail.com>
2018-03-03 09:18:31 -0700

    creates the get route for single note by id

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,17 @@
+var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
-  const collection =
-  app.post('/notes', (req, res) => {
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
       if (err) {
